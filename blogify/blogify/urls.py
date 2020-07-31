5358a34390f13f0124a356b8ba24c610df41d7e9
4-bytes <4-bytes>
2020-07-31 11:59:23 +0100

    :pencil: updated urls

--- blogify/blogify/urls.py
+++ blogify/blogify/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')), # include the accounts urls from the accounts app
     path('posts/', include('posts.urls')), # include the posts.urls from the posts app
     path('about/', views.about),
     path('', views.home), # r'^$'
