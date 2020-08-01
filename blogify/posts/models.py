55e6a4abf04cc65be6d06fdff8f25ab4936d62c1
4-bytes <4-bytes>
2020-08-02 00:28:47 +0100

    :sparkles: post author

--- blogify/posts/models.py
+++ blogify/posts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    # author = 
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True) # auto set time when post is created
-    image = models.ImageField(default="default.png", blank=True)
+    image = models.ImageField(default="default.jpeg", blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
 
     def __str__(self):
         return self.title
