98677bda40998ead18f6d94d78bc786ea43d611c
4-bytes <4-bytes>
2020-08-02 00:31:34 +0100

    :white_check_mark: revised urls + views

--- blogify/posts/urls.py
+++ blogify/posts/urls.py
@@ -6,5 +6,6 @@ app_name = "posts" # namespace
 
 urlpatterns = [
     path('', views.post_list, name="list"),
+    path('create/', views.post_create, name="create"), # fires this url first instead of slug
     path('<slug:slug>/', views.post_detail, name="detail"),
 ]
--- blogify/posts/views.py
+++ blogify/posts/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
+from . import forms
 
 # Create your views here.
 
@@ -12,3 +14,16 @@ def post_list(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_detail.html', {'post' : post }) # retrieves content for each post
+
+@login_required(login_url="/accounts/login/")
+def post_create(request):
+    if request.method == "POST": # when the submit btn is pressed
+        form = forms.CreatePost(request.POST, request.FILES)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user # set the current logged in user as the post writer
+            instance.save()
+            return redirect('posts:list')
+    else:
+        form = forms.CreatePost()
+    return render(request, "posts/post_create.html", { 'form' : form })
