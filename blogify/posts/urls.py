729da1991919949e67f9d42f93b560b2602d5795
4-bytes <4-bytes>
2020-08-03 13:24:28 +0100

    :globe_with_meridians: updated urls.py

--- blogify/posts/urls.py
+++ blogify/posts/urls.py
@@ -7,5 +7,6 @@ app_name = "posts" # namespace
 urlpatterns = [
     path('', views.post_list, name="list"),
     path('create/', views.post_create, name="create"), # fires this url first instead of slug
+    path('own/', views.post_own, name="own"),
     path('<slug:slug>/', views.post_detail, name="detail"),
 ]
